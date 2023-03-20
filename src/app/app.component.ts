2f74f59869166020f995259da9b53de530c9e925
Yuliya Lamot'ko <yulya31087@gmail.com>
2023-03-20 11:13:11 +0200

    added drag and drop functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'drag-n-drop';
+  images: string[] = [
+    'assets/images/beau_100.jpg',
+    'assets/images/fader_100.jpg',
+    'assets/images/glenda_100.jpg',
+    'assets/images/mofat_100.jpg',
+  ];
+  dropPosition: { x: string; y: string };
+  isHighlighted = false;
+
+  onDragStart(event: DragEvent, image: string): void {
+    event.dataTransfer.setData('image', image);
+  }
+
+  toggleHighlight(): void {
+    this.isHighlighted = !this.isHighlighted;
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
+
+  onDrop(event: DragEvent): void {
+    const image = event.dataTransfer.getData('image');
+    const index = this.images.indexOf(image);
+
+    if (index !== -1) {
+      this.images.splice(index, 1);
+    }
+
+    this.isHighlighted = false;
+    this.showAnimation(event);
+  }
+
+  private showAnimation(event: DragEvent): void {
+    this.dropPosition = {
+      x: event.clientX + 'px',
+      y: event.clientY + 'px',
+    };
+
+    setTimeout(() => {
+      this.dropPosition = null;
+    }, 500);
+  }
 }
